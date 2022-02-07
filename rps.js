22c834985340edaa0a3e4b9473c3c1a90c2e28af
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-02-07 15:11:17 +0700

    add game of five rounds function

--- rps.js
+++ rps.js
@@ -6,19 +6,56 @@ function computerPlay() {
 
 function singleRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection) {
-        return `It's a draw! ${playerSelection.toLowerCase()} can't win nor lose against another ${computerSelection}`
+        return 'draw';
     } else if (
         (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') ||
         (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') ||
         (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock')
         ) {
-        return `You win! ${playerSelection.toLowerCase()} beats ${computerSelection}`
+        return 'player win';
     } else {
-        return `You lose! ${computerSelection} beats ${playerSelection.toLowerCase()}`
+        return 'player lose';
     }
 }
 
-console.log(computerPlay());
-console.log(singleRound('rock', 'rock'));
-console.log(singleRound('rock', 'scissors'));
-console.log(singleRound('rock', 'paper'));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Pick one, rock, papers, or scissors? (If you don't, I'll choose one for you)");
+        if (playerSelection === '' || playerSelection === null) {
+            playerSelection = computerPlay();
+        }
+        let computerSelection = computerPlay();
+
+        console.log(`Player chooses ${playerSelection}`);
+        console.log(`Computer chooses ${computerSelection}`);
+        
+        let round = singleRound(playerSelection, computerSelection);
+
+        if (round === 'draw') {
+            playerScore++;
+            computerScore++;
+        } else if (round === 'player win') {
+            playerScore++;
+        } else {
+            computerScore++;
+        }
+
+        console.log(round);
+        console.log(`Player: ${playerScore}, Computer: ${computerScore}`);
+    }
+
+    if (playerScore === computerScore) {
+        return `It's a draw game! Player has ${playerScore} points and the computer has ${computerScore} points.`;
+    } else if (playerScore > computerScore) {
+        return `Player wins the game! Player has ${playerScore} points and the computer has ${computerScore} points.`;
+    } else {
+        return `Computer wins the game! Player has ${playerScore} points and the computer has ${computerScore} points.`;
+    }
+
+}
+
+const theGame = game();
+console.log(theGame);
