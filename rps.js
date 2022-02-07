aa4c1238d8562d2006d88881a49df113f8f3547c
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-02-07 14:39:05 +0700

    add single round of playing function

--- rps.js
+++ rps.js
@@ -1,7 +1,24 @@
 function computerPlay() {
-    const hand = ['Rock', 'Paper', 'Scissors'];
+    const hand = ['rock', 'paper', 'scissors'];
     const random = Math.floor(Math.random() * hand.length);
     return hand[random];
 }
 
+function singleRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
+        return `It's a draw! ${playerSelection.toLowerCase()} can't win nor lose against another ${computerSelection}`
+    } else if (
+        (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock')
+        ) {
+        return `You win! ${playerSelection.toLowerCase()} beats ${computerSelection}`
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection.toLowerCase()}`
+    }
+}
+
 console.log(computerPlay());
+console.log(singleRound('rock', 'rock'));
+console.log(singleRound('rock', 'scissors'));
+console.log(singleRound('rock', 'paper'));
